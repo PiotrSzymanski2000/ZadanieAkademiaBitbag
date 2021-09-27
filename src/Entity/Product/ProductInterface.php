f34afe841ec2d89f34a28bf0886abd5c2329b19d
PiotrSzymanski2000 <PiotrekSz-18@wp.pl>
2021-09-27 11:46:14 +0200

    remove default entities & add color to Product

---
+++ src/Entity/Product/ProductInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Entity\Product;
+
+interface ProductInterface
+{
+    public  const PRODUCT_COLOR_RED = "Red";
+    public  const PRODUCT_COLOR_BLUE = "Blue";
+    public  const PRODUCT_COLOR_GREEN = "Green";
+
+    public function getColor(): ?string;
+
+    public function setColor(?string $color): void;
+}
