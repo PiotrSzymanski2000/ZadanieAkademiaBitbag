f9422ab2af6356ad7ba061b86bee6299fd7506e5
PiotrSzymanski2000 <PiotrekSz-18@wp.pl>
2021-09-27 15:10:16 +0200

    Fix 5

---
+++ src/Migrations/Version20210927150432.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20210927150432 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'add color to sylius product';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE sylius_product ADD color VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE sylius_product DROP color');
+    }
+}
